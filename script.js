8974e194fcb0296685d505f1b6b0f82c6af605cb
joaoramalho9 <145987843+joaoramalho9@users.noreply.github.com>
2023-12-02 20:20:00 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// JavaScript Document
+
+
+let name = "João Ramalho";
+let idade = 19; 
+
+console.log(name);
+console.log(idade);
+
+let idadeMultiplicada = idade * 7;
+console.log(idadeMultiplicada);
+
+let Conhecidos = [
+  {
+    name: "Rúben",
+    age: 20,
+    isStudent: false
+  },
+  {
+    name: "Eusébio",
+    age: 77,
+    isStudent: true
+  },
+  {
+    name: "Alberto João Jardim",
+    age: 85,
+    isStudent: false
+  }
+];
+
+
+console.log("Caracteristicas dos Conhecidos: ",Conhecidos);
+
+
+var nomesdeConhecidos = ["Rúben", "Eusébio", "Alberto João Jardim"];
+console.log("Nomes dos amigos:", nomesdeConhecidos);
+
+var amigo1 = {
+    nome: "Rúben",
+    idade: 20,
+    estudante: true
+  };
+
+  var amigo2 = {
+    nome: "Eusébio",
+    idade: 77,
+    estudante: false,
+  };
+
+  var amigo3 = {
+    nome: "Alberto João Jardim",
+    idade: 85,
+    estudante: false,
+  };
+
+  console.log("amigo1: ",amigo1);
+  console.log("amigo2: ",amigo2);
+  console.log("amigo3: ",amigo3);
